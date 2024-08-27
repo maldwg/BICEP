e81127d3facc8bd42f73327ed69eac0d7a9998a6
maldwg <maxludwig3@t-online.de>
2024-08-27 19:14:07 +0200

    Embed dashboard into frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
     {path: "", loadComponent: () => import("./dashboard/dashboard.component").then(mod => mod.DashboardComponent)},
     {path: "config", loadComponent: () => import("./config/config.component").then(mod => mod.ConfigComponent)},
     {path: "setup", loadComponent: () => import("./setup/setup.component").then(mod => mod.SetupComponent)},
+    {path: "metrics", loadComponent: () => import("./metrics/metrics.component").then(mod => mod.MetricsComponent)},
 
 ];
