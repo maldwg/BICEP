7a7a7fbeb53fb79697e288c6698e62695a7b044c
maldwg <maxludwig3@t-online.de>
2024-08-31 14:11:13 +0200

    adjust dashboard overview on ensembles and containers

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: "", loadComponent: () => import("./dashboard/dashboard.component").then(mod => mod.DashboardComponent)},
-    {path: "config", loadComponent: () => import("./config/config.component").then(mod => mod.ConfigComponent)},
+    {path: "upload", loadComponent: () => import("./config/config.component").then(mod => mod.ConfigComponent)},
     {path: "setup", loadComponent: () => import("./setup/setup.component").then(mod => mod.SetupComponent)},
     {path: "metrics", loadComponent: () => import("./metrics/metrics.component").then(mod => mod.MetricsComponent)},
 
