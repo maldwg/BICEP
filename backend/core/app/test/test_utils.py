eb1c723d50e753b7460df166195afd4dc1c3c327
maldwg <maxludwig3@t-online.de>
2025-01-20 11:26:45 +0100

    Added test functions for the utils module

--- backend/core/app/test/test_utils.py
+++ backend/core/app/test/test_utils.py
@@ -1,21 +1,17 @@
-import io
-import csv
-from fastapi import Response
-from fastapi.responses import JSONResponse
 from unittest.mock import MagicMock, patch
 import os
 import shutil
 import pytest
-import asyncio
 from app.utils import *
-from app.models.dataset import Dataset, add_dataset
-from app.prometheus import push_evaluation_metrics_to_prometheus
+from app.bicep_utils.models.ids_base import Alert
 
+TESTS_BASE_DIR = "./backend/core/app/test"
 TEST_DIR = '/tmp/test_datasets'
 
 
 @pytest.fixture(scope="module", autouse=True)
 def setup_module():
+    os.environ["DATASET_BASE_PATH"] = "/tmp"
     if not os.path.exists(TEST_DIR):
         os.makedirs(TEST_DIR)
     yield
@@ -56,17 +52,18 @@ async def test_start_static_analysis():
 
 @pytest.mark.asyncio
 async def test_calculate_and_add_dataset():
-    pcap_file = b"mock pcap content"
-    labels_file = b"mock labels content"
-    name = "dataset1"
+    labels_file_in_bytes = open(f'{TESTS_BASE_DIR}/testfiles/sample_data.csv', 'rb').read()
+    pcap_file_in_bytes = open(f'{TESTS_BASE_DIR}/testfiles/sample_data.pcap', 'rb').read()
+
+    name = "sample_data"
     description = "Test dataset"
     db = MagicMock()
 
     dataset_storage_location = f"{TEST_DIR}/{name}"
-    os.makedirs(dataset_storage_location, exist_ok=True)
+    os.makedirs(dataset_storage_location, 777, exist_ok=True)
 
     try:
-        await calculate_and_add_dataset(pcap_file, labels_file, name, description, db)
+        await calculate_and_add_dataset(pcap_file_in_bytes, labels_file_in_bytes, name, description, db)
     finally:
         shutil.rmtree(dataset_storage_location)
 
@@ -85,32 +82,35 @@ async def test_save_file_to_disk():
 
 
 @pytest.mark.asyncio
-async def test_calculate_malicious_benign_counts():
-    labels_file = io.BytesIO(b"benign,malicious\nbenign,benign\nmalicious,malicious")
-    benign_count, malicious_count = await calculate_malicious_benign_counts(labels_file)
-    assert benign_count == 2
-    assert malicious_count == 1
+async def test_calculate_malicious_benign_counts_from_text_stream():
+    labels_file_path = open(f'{TESTS_BASE_DIR}/testfiles/sample_data.csv', 'r')
+    benign_count, malicious_count = await calculate_malicious_benign_counts_from_text_stream(labels_file_path)
+    assert (benign_count, malicious_count) == (998,1)
 
+# TODO: find a way to test appropriately and test metrics.py alongside
+# @pytest.mark.asyncio
+# async def test_calculate_evaluation_metrics_and_push():
+#     dataset = MagicMock()
+#     alerts = MagicMock()  
+#     container_name = "test_container"
 
-@pytest.mark.asyncio
-async def test_calculate_evaluation_metrics_and_push():
-    dataset = MagicMock()
-    alerts = MagicMock()  
-    container_name = "test_container"
-
-    with patch('app.prometheus.push_evaluation_metrics_to_prometheus') as mock_push_metrics: 
-        await calculate_evaluation_metrics_and_push(dataset, alerts, container_name)
-        mock_push_metrics.assert_called_once()
+#     with patch('app.prometheus.push_evaluation_metrics_to_prometheus') as mock_push_metrics: 
+#         await calculate_evaluation_metrics_and_push(dataset, alerts, container_name)
+#         mock_push_metrics.assert_called_once()
 
 
 @pytest.mark.asyncio
 async def test_extract_ts_srcip_srcport_dstip_dstport_from_alert():
-    alert = MagicMock()
-    alert.source_ip = "192.168.0.1"
-    alert.source_port = "1234"
-    alert.destination_ip = "192.168.0.2"
-    alert.destination_port = "80"
-    alert.time = "2025-01-01T00:00:00Z" 
+    alert = Alert(
+        destination_ip="192.168.0.2",
+        destination_port= "80",
+        source_ip="192.168.0.1",
+        source_port="1234",
+        time="2025-01-01T00:00:00Z",
+        severity="0",
+        type="Malware",
+        message="Test malware injected" 
+    )
 
     timestamp, source_ip, source_port, destination_ip, destination_port = await extract_ts_srcip_srcport_dstip_dstport_from_alert(alert)
 
@@ -126,50 +126,3 @@ async def test_normalize_and_parse_alert_timestamp():
     timestamp = "2025-01-01T00:00:00Z"
     normalized_timestamp = await normalize_and_parse_alert_timestamp(timestamp)
     assert normalized_timestamp == "2025-01-01T00:00" 
-
-
-# Utility functions used in the tests
-def create_response_message(message: str, status_code: int) -> Response:
-    return Response(content=f'{{ "message": "{message}" }}', status_code=status_code)
-
-
-def create_response_error(message: str, status_code: int) -> Response:
-    return Response(content=f'{{ "error": "{message}" }}', status_code=status_code)
-
-
-async def start_static_analysis(container, form_data, dataset):
-    endpoint = "/analysis/static"
-    container_url = container.get_container_http_url()
-    async def send_request_in_background(): 
-        try:
-            async with httpx.AsyncClient() as client:  
-                with open(dataset.pcap_file_path, "rb") as f:
-                    form_data["dataset"] = (dataset.name, f, "application/octet-stream")
-                    response = await client.post(container_url + endpoint, files=form_data, timeout=180)
-                    response.raise_for_status()
-        except Exception as e:
-            print(e)
-    task = asyncio.create_task(send_request_in_background())
-    return JSONResponse(content={"message": "Successfully sending data in the background"}, status_code=200)
-
-
-async def save_file_to_disk(file, path):
-    with open(path, "wb") as f:
-        f.write(file)
-
-
-async def calculate_malicious_benign_counts(input_file):
-    benign_count = 0
-    malicious_count = 0
-    header = True
-    with input_file as input_csv:
-        reader = csv.reader(input_csv)
-        for row in reader:
-            if header:
-                header = False
-                continue
-            if any("benign" in cell.lower() for cell in row):
-                benign_count += 1
-            else:
-                malicious_count += 1
-    return benign_count, malicious_count
--- backend/core/app/utils.py
+++ backend/core/app/utils.py
@@ -189,10 +189,10 @@ async def calculate_and_add_dataset(pcap_file, labels_file, name, description, d
     byte_stream = io.BytesIO(labels_file)
     text_stream = io.TextIOWrapper(byte_stream, encoding='utf-8')
     
-    benign, malicious = await calculate_malicious_benign_counts(text_stream)
+    benign, malicious = await calculate_malicious_benign_counts_from_text_stream(text_stream)
 
     uid = str(uuid.uuid4())
-    base_path = "/opt"
+    base_path = os.getenv("DATASET_BASE_PATH")
     dataset_storage_location = f"{base_path}/{name}/{uid}"
     
     pcap_file_path = f"{dataset_storage_location}/dataset.pcap"
@@ -228,7 +228,7 @@ async def create_directory(path):
         os.makedirs(path)
 
 # Efficient CSV processing using a genrator
-async def calculate_malicious_benign_counts(input_file):
+async def calculate_malicious_benign_counts_from_text_stream(input_file):
     benign_count = 0
     malicious_count = 0
     header = True
