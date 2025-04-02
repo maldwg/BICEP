3e36b2e68a908d4d52c78dbb441145465f8e47da
maldwg <maxludwig3@t-online.de>
2025-04-02 15:10:28 +0200

    Finish documentation draft for readthedocs

--- backend/core/app/models/ensemble_techniques_implementation/sample_vote.py
+++ backend/core/app/models/ensemble_techniques_implementation/sample_vote.py
@@ -1,37 +1,21 @@
 """
-The purpose of this smaple file is to give developers a directive howto implement their own ensembling algorithms
-1. Add a database entry for your new algorithm
-2. create a new python file in this directory here, named exactly as the function_name property you selcted for the database entry
-3. implement an async method called the same as the function_name property you selected
+The following method shows, how to implement a method for an ensembling technique
+    1. Add a database entry for your new algorithm
+    2. create a new python file in this directory here, named exactly as the function_name property you selcted for the database entry
+    3. implement an async method called the same as the function_name property you selected
 """
 from app.bicep_utils.models.ids_base import Alert
 from app.logger import LOGGER
 
 async def sample(alerts_dict: dict, ensemble) -> list[Alert]:
     """
-        The method takes as input the common alerts of an ensemble, as well as the ensemble instance.
-        The format of the alerts_dict alerts is as follows:
-            {
-                ids1: list[Alert],
-                ids2: list[Alert],
-                ....
-            }
+    Method to calculate which alerts of an ensemble are majority voted ones
 
-        You might need to convert this dict into another form, so that you are able to know which alerts are commonly outputted by the IDS and which are not.
-        as an example, the majority vote convertss the alerts_dict to such a structure:
+    Args:
+        alerts_dict (dict): Dict that holds for each IDS in the ensemble a list of alerts
+        ensemble: (Ensemble): Ensemble Object according to the ORM
 
-            {
-                ts-src_ip-src_port-dst_ip-dst_port: {
-                    "ids1": list[Alert],
-                    "ids2": list[Alert]
-                }, 
-                ts2-src_ip2-src_port2-dst_ip2-dst_port2: {
-                    "ids1": list[Alert],
-                    "ids2": list[Alert]
-                }, 
-            }
-        This allows to directly know which alert has been issiued by which IDS, as the keys of the dict are quasi unique.
-        return: Should return a list of Alerts that the ensemble consdiers as agreed upon by the individual IDS
+    Returns:
+        voted_alerts (list[Alert]): List of alerts the ensemble voted for using a specific algorithm
     """
-    # return majority_voted_alerts
     pass
