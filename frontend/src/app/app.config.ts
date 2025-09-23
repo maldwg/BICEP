7de283ba1c715d3252c00263235490c95d9194c2
maldwg <maxludwig3@t-online.de>
2025-09-23 13:32:51 +0200

    Update angular 17 -> 18 part 2

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
