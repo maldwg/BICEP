a0332515833224388d003f2e46fcc0dc0c016bcc
maldwg <maxludwig3@t-online.de>
2026-05-12 15:02:33 +0200

    Fix icon issues

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -3,28 +3,17 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
-import { CommonModule } from '@angular/common';
-
 @Component({
     selector: 'app-navbar',
     imports: [
         MatToolbarModule,
         MatIconModule,
         RouterModule,
-        CommonModule,
         MatMenuModule,
     ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  isMenuOpen = false;
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  closeMenu() {
-    this.isMenuOpen = false;
-  }
 }
